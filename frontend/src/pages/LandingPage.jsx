b80ebe676ec5a2373cc318c8e6e54c770951ba10
Ali Kashif <alikashif2205@gmail.com>
2026-07-09 14:15:13 +0500

    frontend 2

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/HeroSection"
 
 export default function LandingPage() {
   return (
-    <div className="w-full min-h-screen bg-[#1A041C]">
+    <div className="w-full min-h-screen bg-[#FCFBF4]">
       <HeroSection />
     </div>
   )
